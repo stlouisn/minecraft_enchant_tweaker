c95771ddf0ff3b6da451b2cd77ef3e5b418250d0
stlouisn <stlouisn@hotmail.com>
2024-05-11 12:05:38 -0300

    remove default case in protectionType

--- src/main/java/com/adibarra/enchanttweaker/mixin/server/capmod/ProtectionEnchantMixin.java
+++ src/main/java/com/adibarra/enchanttweaker/mixin/server/capmod/ProtectionEnchantMixin.java
@@ -19,7 +19,6 @@ public abstract class ProtectionEnchantMixin {
     @Shadow @Final
     public ProtectionEnchantment.Type protectionType;
 
-    @SuppressWarnings("UnnecessaryDefault")
     @ModifyReturnValue(
         method="getMaxLevel()I",
         at=@At("RETURN"))
@@ -31,7 +30,6 @@ public abstract class ProtectionEnchantMixin {
             case FALL -> ETMixinPlugin.getConfig().getOrDefault("feather_falling", orig);
             case EXPLOSION -> ETMixinPlugin.getConfig().getOrDefault("blast_protection", orig);
             case PROJECTILE -> ETMixinPlugin.getConfig().getOrDefault("projectile_protection", orig);
-            default -> orig;
         };
         if (lvlCap < 0) return orig;
         return ADMath.clamp(lvlCap, 0, 255);
